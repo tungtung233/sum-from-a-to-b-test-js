b75428d073a9c7e557ecbac50db33dd814c7e62c
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-06-26 03:19:22 +0000

    completed the recursive function 'sum'

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+
+  if (fromN === toN) {
+    return fromN;
+  }
+
+  return fromN + sum(fromN + 1, toN);
 }
+
+
+
+console.log(sum(3, 7));
 
 module.exports = sum;
